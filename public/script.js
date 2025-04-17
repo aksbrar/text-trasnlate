d41f07278a2906d4a90dc9bb4ebdc0dd77b3d116
aksbrar <aksbrar34@gmail.com>
2025-04-16 21:08:30 -0400

    gatherd avialbale voices and insert them into options

--- public/script.js
+++ public/script.js
@@ -1,12 +1,25 @@
-// Variables 
+// constant Variables 
 const textInput = document.querySelector("textarea")
 const voice = document.querySelector("#voice")
 const lang = document.querySelector("#language")
-
 const bt = document.querySelector("button")
+
+// loading available voices
+let voices = []
+function loadVoices(){
+  voices = speechSynthesis.getVoices()
+  voice.innerHTML = voices
+    .map((voice, index) => `<option value="${index}">${voice.name} (${voice.lang})</option>`)
+    .join('')
+}
+
+//trigger load voices
+speechSynthesis.onvoiceschanged = loadVoices
+loadVoices()
 
 // speak the text
 bt.addEventListener('click', () => {
   let utternace = new SpeechSynthesisUtterance(textInput.value)
   speechSynthesis.speak(utternace)
 })
+
